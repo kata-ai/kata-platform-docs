861436193ead9967648c0814397a069bb24537da
Yosua Risandy <jojo@kata.ai>
2021-10-21 14:27:20 +0700

    fix[docs]: add custom table by classname

--- src/components/page/Markdown/MarkdownComponents.tsx
+++ src/components/page/Markdown/MarkdownComponents.tsx
@@ -13,22 +13,7 @@ const HorizontalRule = styled('hr')`
   border-top: 1px solid ${colors.grey02};
 `;
 
-export const h1 = (props: JSX.IntrinsicAttributes) => (
-  <Heading size={800} as="h1" fontFamily="sansSerif" color="grey09" {...props} />
-);
-export const h2 = (props: JSX.IntrinsicAttributes) => <Heading size={600} as="h2" mt="xl" color="grey09" {...props} />;
-export const h3 = (props: JSX.IntrinsicAttributes) => <Heading size={500} as="h3" mt="xl" color="grey09" {...props} />;
-export const h4 = (props: JSX.IntrinsicAttributes) => <Heading size={400} as="h4" mt="xl" color="grey09" {...props} />;
-export const h5 = (props: JSX.IntrinsicAttributes) => <Heading size={400} as="h5" mt="xl" color="grey09" {...props} />;
-export const h6 = (props: JSX.IntrinsicAttributes) => <Heading size={400} as="h6" mt="xl" color="grey09" {...props} />;
-export const p = (props: JSX.IntrinsicAttributes) => <Paragraph size={400} mt="sm" color="grey07" {...props} />;
-export const hr = (props: JSX.IntrinsicAttributes) => <HorizontalRule {...props} />;
-export const ul = (props: JSX.IntrinsicAttributes) => <UnorderedList {...props} />;
-export const ol = (props: JSX.IntrinsicAttributes) => <UnorderedList as="ol" color="grey07" {...props} />;
-export const li = (props: JSX.IntrinsicAttributes) => (
-  <Paragraph size={400} as="li" color="grey07" my="xxs" {...props} />
-);
-export const table = styled('table')`
+const TableDefault = styled('table')`
   width: 100%;
   margin: ${space.lg}px 0;
   font-size: ${textSizes[400].fontSize}px;
@@ -102,3 +87,70 @@ export const table = styled('table')`
     }
   }
 `;
+
+const TableBordered = styled('table')`
+  width: 100%;
+  margin: ${space.lg}px 0;
+  font-size: ${textSizes[300].fontSize}px;
+  line-height: ${textSizes[300].lineHeight}px;
+  border-collapse: collapse;
+  border: 2px solid ${colors.grey11};
+
+  thead {
+    border-bottom: 1px solid ${colors.grey02};
+    th {
+      padding: 14px 16px;
+      font-style: normal;
+      font-stretch: normal;
+      font-weight: 700;
+      letter-spacing: -0.01em;
+      text-transform: uppercase;
+      text-align: center;
+      color: ${colors.grey12};
+    }
+  }
+
+  tbody {
+    tr {
+      td {
+        padding: 14px 16px;
+        min-width: 15vw;
+        vertical-align: top;
+        font-size: ${textSizes[400].fontSize}px;
+        line-height: ${textSizes[400].lineHeight}px;
+        color: ${colors.grey12};
+        overflow-wrap: anywhere;
+      }
+      &:not(:last-child){
+        border-bottom: 1px solid ${colors.grey11};
+      }
+    }
+  }
+`;
+
+export const h1 = (props: JSX.IntrinsicAttributes) => (
+  <Heading size={800} as="h1" fontFamily="sansSerif" color="grey09" {...props} />
+);
+export const h2 = (props: JSX.IntrinsicAttributes) => <Heading size={600} as="h2" mt="xl" color="grey09" {...props} />;
+export const h3 = (props: JSX.IntrinsicAttributes) => <Heading size={500} as="h3" mt="xl" color="grey09" {...props} />;
+export const h4 = (props: JSX.IntrinsicAttributes) => <Heading size={400} as="h4" mt="xl" color="grey09" {...props} />;
+export const h5 = (props: JSX.IntrinsicAttributes) => <Heading size={400} as="h5" mt="xl" color="grey09" {...props} />;
+export const h6 = (props: JSX.IntrinsicAttributes) => <Heading size={400} as="h6" mt="xl" color="grey09" {...props} />;
+export const p = (props: JSX.IntrinsicAttributes) => <Paragraph size={400} mt="sm" color="grey07" {...props} />;
+export const hr = (props: JSX.IntrinsicAttributes) => <HorizontalRule {...props} />;
+export const ul = (props: JSX.IntrinsicAttributes) => <UnorderedList {...props} />;
+export const ol = (props: JSX.IntrinsicAttributes) => <UnorderedList as="ol" color="grey07" {...props} />;
+export const li = (props: JSX.IntrinsicAttributes) => (
+  <Paragraph size={400} as="li" color="grey07" my="xxs" {...props} />
+);
+
+interface TableAttributes extends JSX.IntrinsicAttributes{
+  className?: string;
+}
+export const table = (props: TableAttributes) => {
+  const { className, ...rest } = props;
+  if (className?.includes('bordered')){
+    return <TableBordered {...rest} />
+  }
+  return <TableDefault {...rest} />
+}
